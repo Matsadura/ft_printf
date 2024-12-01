2bf92839def4a10004110226926777f6d4c03dfc
Matsadura <zidanditoo1@gmail.com>
2024-12-01 15:55:46 +0100

    [FIX] Norminette

--- ft_printf.h
+++ ft_printf.h
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zzaoui <zzaoui@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 19:18:17 by zzaoui            #+#    #+#             */
-/*   Updated: 2024/11/30 21:21:04 by zzaoui           ###   ########.fr       */
+/*   Updated: 2024/12/01 15:52:26 by zzaoui           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-//# include "libft/libft.h"
 # include <stdarg.h>
 # include <stddef.h>
 # include <stdlib.h>
@@ -27,25 +26,25 @@ typedef struct s_spec
 	int		(*f)(va_list);
 }	t_spec;
 
-int	ft_printf(const char *str, ...);
+int		ft_printf(const char *str, ...);
 
 /* Structure related functions */
-int	init(t_spec **spec);
-int	get_spec_func(char s, t_spec *sp, va_list arg);
+int		init(t_spec **spec);
+int		get_spec_func(char s, t_spec *sp, va_list arg);
 
 /* Specificer functions */
-int	put_mod(va_list arg);
-int	put_char(va_list arg);
-int	put_str(va_list arg);
-int	put_nbr(va_list arg);
-int	put_unbr(va_list arg);
-int	put_xlower(va_list arg);
-int	put_xupper(va_list arg);
-int	put_p(va_list arg);
+int		put_mod(va_list arg);
+int		put_char(va_list arg);
+int		put_str(va_list arg);
+int		put_nbr(va_list arg);
+int		put_unbr(va_list arg);
+int		put_xlower(va_list arg);
+int		put_xupper(va_list arg);
+int		put_p(va_list arg);
 
 /* Helpers */
-size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
 char	*ft_itoa(int n);
+size_t	ft_strlen(const char *str);
 
 #endif /* FT_PRINTF_H */
